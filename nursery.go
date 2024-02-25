fd49436dfe42c70c280c85942b4a4dfdb83cbab9
bakalover <kostya@shprenger.com>
2024-02-26 00:49:36 +0300

    Tree-Like Nursery

--- nursery.go
+++ nursery.go
@@ -2,19 +2,57 @@ package tate
 
 import "sync"
 
-type Nursery struct {
-	wg sync.WaitGroup
+type Chainer struct {
+	toJoin      []Joinable
+	routineRefs sync.WaitGroup
 }
 
-func (n *Nursery) Add(routine func()) *Nursery {
+func (c *Chainer) AddRef() {
+	c.routineRefs.Add(1)
+}
+
+func (c *Chainer) ReleaseRef() {
+	c.routineRefs.Done()
+}
+
+func (c *Chainer) Link(js ...Joinable) {
+	c.toJoin = append(c.toJoin, js...)
+}
+
+func (c *Chainer) Join() {
+	c.routineRefs.Wait()
+	for _, js := range c.toJoin {
+		js.Join()
+	}
+}
+
+type Nursery struct {
+	wg sync.WaitGroup
+	cn Chainer
+}
+
+func NewNursery(cn *Chainer) *Nursery {
+	nr := &Nursery{}
+	if cn != nil {
+		cn.Link(nr)
+	}
+	return nr
+}
+
+func (n *Nursery) Add(routine func(c *Chainer)) *Nursery {
 	n.wg.Add(1)
+	n.cn.AddRef()
 	go func() {
-		routine()
-		n.wg.Done()
+		defer n.wg.Done()
+		routine(&n.cn)
+		
+		// At this point we have strong knowledge about all subsciptions on Chainer 
+		n.cn.ReleaseRef()
 	}()
 	return n
 }
 
 func (n *Nursery) Join() {
+	n.cn.Join()
 	n.wg.Wait()
 }
--- test/nursery_test.go
+++ test/nursery_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/bakalover/tate"
 )
@@ -10,21 +11,21 @@ import (
 const IterNursery = 123125
 
 func TestNurseryJustWork(t *testing.T) {
-	var nr tate.Nursery
+	nr := tate.NewNursery(nil)
 	var p1, p2 = false, false
-	nr.Add(func() { p1 = true }).Add(func() { p2 = true }).Join()
+	nr.Add(func(c *tate.Chainer) { p1 = true }).Add(func(c *tate.Chainer) { p2 = true }).Join()
 	if !p1 || !p2 {
 		t.Fatal()
 	}
 }
 
 func TestNurseryForgetToAdd(t *testing.T) {
-	var nr tate.Nursery
+	nr := tate.NewNursery(nil)
 	nr.Join()
 }
 
 func TestNurseryJoins(t *testing.T) {
-	var nr tate.Nursery
+	nr := tate.NewNursery(nil)
 	nr.Join()
 	nr.Join()
 	nr.Join()
@@ -32,22 +33,22 @@ func TestNurseryJoins(t *testing.T) {
 }
 
 func TestNurserySeveralJoins(t *testing.T) {
-	var nr tate.Nursery
+	nr := tate.NewNursery(nil)
 	var p1, p2, p3 = false, false, false
 
-	nr.Add(func() { p1 = true })
+	nr.Add(func(c *tate.Chainer) { p1 = true })
 	nr.Join()
 	if !p1 {
 		t.Fatal()
 	}
 
-	nr.Add(func() { p2 = true })
+	nr.Add(func(c *tate.Chainer) { p2 = true })
 	nr.Join()
 	if !p2 {
 		t.Fatal()
 	}
 
-	nr.Add(func() { p3 = true })
+	nr.Add(func(c *tate.Chainer) { p3 = true })
 	nr.Join()
 	if !p3 {
 		t.Fatal()
@@ -55,12 +56,12 @@ func TestNurserySeveralJoins(t *testing.T) {
 }
 
 func TestNurseryGroupStart(t *testing.T) {
-	var nr tate.Nursery
+	nr := tate.NewNursery(nil)
 	var mutex sync.Mutex
 	var counter = 0
 
 	for i := 0; i < IterNursery; i++ {
-		nr.Add(func() {
+		nr.Add(func(c *tate.Chainer) {
 			mutex.Lock()
 			counter++
 			mutex.Unlock()
@@ -73,12 +74,12 @@ func TestNurseryGroupStart(t *testing.T) {
 }
 
 func TestNurseryEachWait(t *testing.T) {
-	var nr tate.Nursery
+	nr := tate.NewNursery(nil)
 	var mutex sync.Mutex
 	var counter = 0
 
 	for i := 0; i < IterNursery; i++ {
-		nr.Add(func() {
+		nr.Add(func(c *tate.Chainer) {
 			mutex.Lock()
 			counter++
 			mutex.Unlock()
@@ -92,14 +93,14 @@ func TestNurseryEachWait(t *testing.T) {
 }
 
 func TestNurseryBatchWait(t *testing.T) {
-	var nr tate.Nursery
+	nr := tate.NewNursery(nil)
 	var mutex sync.Mutex
 	var counter = 0
 	var kBatch = 25
 
 	for i := 0; i < kBatch; i++ {
 		for i := 0; i < IterNursery/kBatch; i++ {
-			nr.Add(func() {
+			nr.Add(func(c *tate.Chainer) {
 				mutex.Lock()
 				counter++
 				mutex.Unlock()
@@ -109,6 +110,118 @@ func TestNurseryBatchWait(t *testing.T) {
 	}
 
 	if !(counter == IterNursery) {
+		t.Fatal()
+	}
+}
+
+func TestChain(t *testing.T) {
+	nr := tate.NewNursery(nil)
+	p := false
+	nr.Add(func(c *tate.Chainer) {
+		nrInner := tate.NewNursery(c)
+		nrInner.Add(func(c *tate.Chainer) { p = true })
+	})
+	nr.Join()
+	if !p {
+		t.Fatal()
+	}
+}
+
+func TestLongSubsription(t *testing.T) {
+	nr := tate.NewNursery(nil)
+	p := false
+	nr.Add(func(c *tate.Chainer) {
+		nrInner := tate.NewNursery(c)
+		nrInner.Add(func(c *tate.Chainer) {
+			time.Sleep(5 * time.Second)
+			p = true
+		})
+	})
+	nr.Join()
+	if !p {
+		t.Fatal()
+	}
+}
+
+func TestStrangeApi(t *testing.T) {
+	nr := tate.NewNursery(nil)
+	p := false
+	nr.Add(func(c *tate.Chainer) {
+		nrInner := tate.NewNursery(c)
+		nrInner.Join()
+		nrInner.Add(func(c *tate.Chainer) {
+			p = true
+		})
+		nrInner.Join()
+		nrInner.Join()
+		nrInner.Join()
+	})
+	nr.Join()
+	if !p {
+		t.Fatal()
+	}
+}
+
+func TestTelescope(t *testing.T) {
+	nr1 := tate.NewNursery(nil)
+	p := false
+	nr1.Add(func(c1 *tate.Chainer) {
+		nr2 := tate.NewNursery(c1)
+		nr2.Add(func(c2 *tate.Chainer) {
+			nr3 := tate.NewNursery(c2)
+			nr3.Add(func(c3 *tate.Chainer) {
+				nr4 := tate.NewNursery(c3)
+				nr4.Add(func(c4 *tate.Chainer) {
+					nr5 := tate.NewNursery(c4)
+					nr5.Add(func(c5 *tate.Chainer) {
+						nr6 := tate.NewNursery(c5)
+						nr6.Add(func(c6 *tate.Chainer) {
+							p = true
+						})
+					})
+				})
+			})
+		})
+	})
+	nr1.Join()
+	if !p {
+		t.Fatal()
+	}
+}
+
+func TestConcurrentTree(t *testing.T) {
+	nr := tate.NewNursery(nil)
+	var mutex sync.Mutex
+	var counter = 0
+
+	nr.Add(func(c *tate.Chainer) {
+		nrInner1 := tate.NewNursery(c)
+		nrInner2 := tate.NewNursery(c)
+		nrInner3 := tate.NewNursery(c)
+		nrInner1.Add(func(c1 *tate.Chainer) {
+			for i := 0; i < IterNursery; i++ {
+				mutex.Lock()
+				counter += 1
+				mutex.Unlock()
+			}
+		})
+		nrInner2.Add(func(c2 *tate.Chainer) {
+			for i := 0; i < IterNursery; i++ {
+				mutex.Lock()
+				counter += 1
+				mutex.Unlock()
+			}
+		})
+		nrInner3.Add(func(c3 *tate.Chainer) {
+			for i := 0; i < IterNursery; i++ {
+				mutex.Lock()
+				counter += 1
+				mutex.Unlock()
+			}
+		})
+	})
+	nr.Join()
+	if counter != IterNursery*3 {
 		t.Fatal()
 	}
 }
