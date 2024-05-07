743817201d133ba3f7bd4cc02bbdfff640391b62
bakalover <kostya@shprenger.com>
2024-05-07 17:55:53 +0300

    Go

--- go.go
+++ go.go
@@ -1,6 +1,9 @@
 package tate
 
-import "sync"
+import (
+	"sync"
+	"sync/atomic"
+)
 
 type Gorroutine struct {
 	routine func()
@@ -20,15 +23,21 @@ func (g *Gorroutine) Wait() {
 }
 
 type JoinHandle struct {
-	gorr *Gorroutine
+	gorr    *Gorroutine
+	isFired atomic.Bool
 }
 
-func (h JoinHandle) Join() {
-	h.gorr.Wait()
+func (h *JoinHandle) Join() {
+	if !h.isFired.CompareAndSwap(false, true) {
+		h.gorr.Wait()
+	} else {
+		panic("Double join on single goroutine!")
+	}
 }
 
+// Cancellation??
 func Go(routine func()) JoinHandle {
 	g := &Gorroutine{routine: routine}
 	g.Launch()
-	return JoinHandle{g}
+	return JoinHandle{g, atomic.Bool{}}
 }
